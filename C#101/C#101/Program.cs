e6a8a4b10e37fe5cb3f91cade96d212fa724530e
therealkhansolo <116898907+therealkhansolo@users.noreply.github.com>
2025-04-28 18:16:15 +0200

    Day 2
    Uploaded exercises done so far & first OOP assignment attempt.

---
+++ C#101/C#101/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace C_101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# 101 with Mr Khan!");
+
+
+            /*C# is a case-sensitive language
+            int number = 3;
+            int Number = 3;
+              */
+
+
+            Console.ReadKey();
+        }
+    }
+}
